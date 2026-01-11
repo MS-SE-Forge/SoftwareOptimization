510d13a87ef5f166d7a53e0a0d0b9e256bef33e7
asiwajudev <olanrewajuranda@gmail.com>
2026-01-11 17:04:59 +0100

    Downgrade Prisma from v7 to v6 and add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { VulnerableController } from './vulnerable.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController, VulnerableController],
   providers: [AppService],
 })
