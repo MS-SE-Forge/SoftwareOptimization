88e92138f8f87514a638512d4fb8a26f6ecadc46
asiwajudev <olanrewajuranda@gmail.com>
2026-01-11 16:17:09 +0100

    Add Swagger/OpenAPI documentation to API endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,33 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { Prisma } from '@prisma/client';
 // import { AuthGuard } from '@nestjs/passport'; // To be implemented
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 201, description: 'User successfully created.' })
   @Post()
   create(@Body() createUserDto: Prisma.UserCreateInput) {
     return this.usersService.create(createUserDto);
   }
 
   // @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return all users.' })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
   // @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ summary: 'Get user by email' })
+  @ApiResponse({ status: 200, description: 'Return user found by email.' })
+  @ApiResponse({ status: 404, description: 'User not found.' })
   @Get(':email')
   findOne(@Param('email') email: string) {
     return this.usersService.findOne(email);
