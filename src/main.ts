13f11ccbf350d737013ccaeeeaf941c1b6309501
asiwajudev <olanrewajuranda@gmail.com>
2026-01-11 20:16:08 +0100

    Add Bearer authentication to Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     .setTitle('Software Optimization API')
     .setDescription('The Software Optimization API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
